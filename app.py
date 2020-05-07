d5395b36ea3f5d80573ad7c902609da66122df62
iqbalbasyar <iqbalbasyar.m@gmail.com>
2020-05-07 17:08:22 +0700

    Solve mac matplotlib assertion error
    Add matplotlib.use('Agg') to avoid the assertion error

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ import matplotlib.ticker as mtick
 from io import BytesIO
 import base64
 
-from helper import load_telco
+import matplotlib
+matplotlib.use('Agg')
+
+# from helper import load_telco, table_churn
+from helper import *
 
 app = Flask(__name__)
 
@@ -16,35 +20,35 @@ def index():
 	# copy data as raw
 	raw = data.copy()
 
-	# # generate value for cards
-	# ## churn rate & retaining customers
-	# table_churn_res = table_churn(raw)
-	# percent_churn = table_churn_res.loc['_______', '_______'].round(2)
-	# percent_retain = _______
+	# generate value for cards
+	## churn rate & retaining customers
+	table_churn_res = table_churn(raw)
+	percent_churn = table_churn_res.loc['Yes', 'percent'].round(2)
+	percent_retain = table_churn_res.loc['No', 'percent'].round(2)
     
-	# ## average lifetime value
-	# average_cltv = int(_______)
+	## average lifetime value
+	average_cltv = int((raw['cltv'].mean()))
     
-	# # compile card values as `card_data`
-	# card_data = dict(
-	# 		percent_churn = f'{percent_churn}%',
-	# 		percent_retain = f'{percent_retain}%',
-	# 		average_cltv = f'{average_cltv:,}'
-	# 	)
+	# compile card values as `card_data`
+	card_data = dict(
+			percent_churn = f'{percent_churn}%',
+			percent_retain = f'{percent_retain}%',
+			average_cltv = f'{average_cltv:,}'
+		)
 
-	# # generate plot
-	# plot_phone_res = plot_phone(raw)
-	# plot_internet_res = plot_internet(raw)
-	# plot_tenure_cltv_res = plot_tenure_cltv(raw)
-	# plot_tenure_churn_res = plot_tenure_churn(raw)
+	# generate plot
+	plot_phone_res = plot_phone(raw)
+	plot_internet_res = plot_internet(raw)
+	plot_tenure_cltv_res = plot_tenure_cltv(raw)
+	plot_tenure_churn_res = plot_tenure_churn(raw)
 
 	# render to html
 	return render_template('index.html',
-		   card_data = None, 
-		#  plot_phone_res=plot_phone_res,
-		#  plot_internet_res=plot_internet_res,
-		#  plot_tenure_cltv_res=plot_tenure_cltv_res,
-		#  plot_tenure_churn_res=plot_tenure_churn_res
+		   card_data = card_data, 
+		 plot_phone_res=plot_phone_res,
+		 plot_internet_res=plot_internet_res,
+		 plot_tenure_cltv_res=plot_tenure_cltv_res,
+		 plot_tenure_churn_res=plot_tenure_churn_res
 		)
 
 
